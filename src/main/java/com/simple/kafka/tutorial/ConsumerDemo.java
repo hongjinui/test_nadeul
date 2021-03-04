b14c03a11e1a4f92380428be9abd74cb3dd557b5
hongjinui <houago3@gmail.com>
2021-03-04 16:20:01 +0900

    test_23

--- src/main/java/com/simple/kafka/tutorial/ConsumerDemo.java
+++ src/main/java/com/simple/kafka/tutorial/ConsumerDemo.java
@@ -12,8 +12,7 @@ import java.util.Properties;
 public class ConsumerDemo {
 
     public static void main(String[] args) {
-        무엇이 에러인가
-
+        이것인가
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
         String bootstrapServer  = "127.0.0.1:9092";
