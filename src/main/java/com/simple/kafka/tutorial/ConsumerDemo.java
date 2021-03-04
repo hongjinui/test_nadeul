378a38efebfb10bf9b3a5b0e4b4110d699d49e39
hongjinui <houago3@gmail.com>
2021-03-04 16:54:08 +0900

    test_35

--- src/main/java/com/simple/kafka/tutorial/ConsumerDemo.java
+++ src/main/java/com/simple/kafka/tutorial/ConsumerDemo.java
@@ -12,7 +12,7 @@ import java.util.Properties;
 public class ConsumerDemo {
 
     public static void main(String[] args) {
-        int a = "홍진의짱짱";
+        드디어
 
 
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
